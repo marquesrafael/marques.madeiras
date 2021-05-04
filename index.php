8bd1094497ba67f4463c8973197da51dca77ed6b
Rafael Marques <smarques.rafael@gmail.com>
2021-05-04 14:50:29 -0300

    Primeiros páginas criadas
    Utilizado as tecnologias:
    - HTML
    - JAVASCRIPT
    
    E os frameworks:
    - BOOTSTRAP
    - FONTAWESOME
    - LIGHTBOX
    
    Futuramente sendo integrado:
    - JQUERY
    - VUE.js

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Marques Madeiras</title>
+
+  <link rel="stylesheet" href="assets/dist/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/global.css" />
+  <link rel="shortcut icon" href="assets/img/favicons/favicon.ico" />
+  <link href="assets/dist/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+  <meta name="author" content="Rafael Marques" />
+  <meta name="contact" content="madeira@rafaelmarques.net" />
+  <meta name="description" content="Página para divulgação de trabalhos feito em madeira" />
+  <meta name="keywords" content="venda, madeira, artesão, manual, marceneiro" />
+  <meta name="copyright" content="© 2021 Rafael Marques" />
+
+  <meta name="robots" content="index, follow" />
+  <meta name="googlebot" content="index, follow" />
+</head>
+
+<body>
+  <div class="container col-8 ">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="assets/img/carros/carro.png
+        " class="d-block mx-lg-auto img-fluid" alt="Carro de madeira, versão calhambeque" width="500" height="300" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Marques Madeiras</h1>
+        <p class="lead">Produtos feito manualmente, em madeira!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+  <div class="container">
+    <div class="row">
+      <a href="assets/img/carros/car7.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car7.jpg" class="img-fluid rounded">
+      </a>
+      <a href="assets/img/carros/car2.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car2.jpg" class="img-fluid rounded">
+      </a>
+      <a href="assets/img/carros/car3.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car3.jpg" class="img-fluid rounded">
+      </a>
+    </div>
+    <div class="row">
+      <a href="assets/img/carros/car4.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car4.jpg" class="img-fluid rounded">
+      </a>
+      <a href="assets/img/carros/car5.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car5.jpg" class="img-fluid rounded">
+      </a>
+      <a href="assets/img/carros/car6.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car6.jpg" class="img-fluid rounded">
+      </a>
+    </div>
+    <div class="row">
+      <a href="assets/img/carros/car8.jpg" data-lightbox="image-1" data-title="Carro em madeira" class="col-md-4">
+        <img src="assets/img/carros/car8.jpg" class="img-fluid rounded">
+      </a>
+    </div>
+  </div>
+
+  <br /><br />
+
+  <main id="content" role="main">
+    <!-- Hero Section -->
+    <div class="position-relative">
+      <div class="bg-img-hero" style="background-image: url(./assets/svg/components/abstract-shapes-12.svg);">
+        <div class="container space-top-3 space-top-lg-4 space-bottom-2 position-relative z-index-2">
+          <div class="w-md-80 w-lg-60 text-center mx-md-auto mb-5 mb-md-9">
+            <h1>Ficou interessado?</h1>
+            <p>Entre em contato conosco para mais informações sobre disponibilidade e preços. Será um prazer atende-los!</p>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4 mb-3 mb-md-0 mb-md-n11">
+              <!-- Card -->
+              <a class="card text-center h-100 transition-3d-hover" href="https://wa.me/5513991645398">
+                <div class="card-body p-lg-5">
+                  <i class="fab fa-whatsapp fa-4x"></i>
+                  <h3 class="h4">Whatsapp</h3>
+                </div>
+              </a>
+              <!-- End Card -->
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0 mb-md-n11">
+              <!-- Card -->
+              <a class="card text-center h-100 transition-3d-hover" href="mailto:madeiras@rafaelmarques.net">
+                <div class="card-body p-lg-5">
+                  <i class="fas fa-envelope-open-text  fa-4x"></i>
+                  <h3 class="h4">Email</h3>
+                </div>
+              </a>
+              <!-- End Card -->
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0 mb-md-n11">
+              <!-- Card -->
+              <a class="card text-center h-100 transition-3d-hover" href="https://www.instagram.com/marques.madeiras/">
+                <div class="card-body p-lg-5">
+                  <i class="fab fa-instagram fa-4x"></i>
+                  <h3 class="h4">Instagram</h3>
+                </div>
+              </a>
+              <!-- End Card -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <footer class="rodape">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-4 text-lg-center site-logo order-1 order-lg-2 mb-3 mb-lg-0">
+          <a href="#" class="m-0 p-0">Marques Madeiras</a>
+        </div>
+        <div class="col-lg-4 order-2 order-lg-1 mb-3 mb-lg-0">
+          <ul class="list-unstyled nav-links m-0 nav-left">
+            <li><a href="https://www.instagram.com/marques.madeiras/">Instagram</a></li>
+            <li><a href="https://wa.me/5513991645398">Whatsapp</a></li>
+            <li><a href="mailto:madeiras@rafaelmarques.net">Email</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-4 text-lg-right order-3 order-lg-3">
+          <p class="m-0 text-muted"><small>&copy; 2021. Fornecido por <a href="https://rafaelmarques.net/">Rafael Marques</a></small></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src="assets/dist/jquery/jquery-3.6.0.min.js"></script>
+  <script src="assets/dist/vuejs/vuejs.js"></script>
+  <script src="assets/dist/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/2c3fcb53c8.js" crossorigin="anonymous"></script>
+  <script src="assets/js/global.js"></script>
+  <script src="assets/dist/lightbox/js/lightbox.min.js"></script>
+
+</body>
+
+</html>
